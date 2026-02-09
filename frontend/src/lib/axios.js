1f3c2877c13a49d549c8c153dec3bc504cf0e6c3
H Navaneeth Dange <hndange@gmail.com>
2026-02-09 21:01:44 +0530

    fe

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://13.201.87.111:5001/api",
   withCredentials: true,
 },
-console.log(`Axios instance created with baseURL: ${import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api"}`));
+console.log(`Axios instance created with baseURL: ${import.meta.env.VITE_API_BASE_URL || "http://13.201.87.111:5001/api"}`));
